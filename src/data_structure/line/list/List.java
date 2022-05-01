01044b47ddc0b32960a7f454ea4b1fcd12de521c
xtremeglory@163.com <342426iamgod>
2022-05-01 22:29:21 +0800

    1.新增线性表实现

--- src/data_structure/line/list/List.java
+++ src/data_structure/line/list/List.java
@@ -1,4 +1,23 @@
 package data_structure.line.list;
 
-public interface List {
+public interface List<E> {
+    void insert(int index, E e);
+
+    void insertLast(E e);
+
+    E remove(int index);
+
+    E removeLast();
+
+    void update(int index, E e);
+
+    E get(int index);
+
+    E getLast();
+
+    int size();
+
+    int indexOf(E e);
+
+    boolean isEmpty();
 }
